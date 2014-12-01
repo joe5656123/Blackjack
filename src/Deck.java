c75567d6837a683d9b59dd89c3ae757c43ce9181
Joe Longobardi <joe5656123@aol.com>
2014-12-01 18:01:08 -0500

    Removed Header
    Removed Header

--- src/Deck.java
+++ src/Deck.java
@@ -1,15 +1,4 @@
-
 import java.util.Collections;
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-/**
- *
- * @author joe56_000
- */
 public class Deck {
     private java.util.ArrayList<Card> _deck;
     
